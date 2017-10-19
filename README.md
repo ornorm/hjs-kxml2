854cddaed4d787801f1c7b3b693b042467ddb7fd
biendo <aime@awelty.com>
2017-10-19 09:11:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# HJS-KXML2
+> KXML2 pull parser classes for the Hubrisjs javascript framework.
+
+kXML2 implementation classes: KXmlParser, KXmlSerializer, KNode, KElement, KDocument.
+
+## Installation
+
+Node:
+
+```sh
+npm install hjs-kxml2 --save
+```
+
+## Meta
+
+Aime – abiendo@gmail.com
+
+Distributed under the MIT license. See ``LICENSE`` for more information.
---
+++ index.js
@@ -0,0 +1,29 @@
+/** @babel */
+import {
+    UNEXPECTED_EOF,
+    ILLEGAL_TYPE,
+    XML_DECL,
+    LEGACY,
+    KXmlParser,
+    KXmlSerializer} from './lib/io';
+import {
+    DOCUMENT,
+    ELEMENT,
+    KNode,
+    KElement,
+    KDocument} from './lib/kdom';
+
+export {
+    UNEXPECTED_EOF,
+    ILLEGAL_TYPE,
+    XML_DECL,
+    LEGACY,
+    KXmlParser,
+    KXmlSerializer,
+
+    DOCUMENT,
+    ELEMENT,
+    KNode,
+    KElement,
+    KDocument
+}
